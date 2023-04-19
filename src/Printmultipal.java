c4f899cd7c39654127f1f041bb744d4ba631dd10
pooja <h.pooja368@gmail.com>
2023-04-19 23:34:13 +0100

    first commit

---
+++ src/Printmultipal.java
@@ -0,0 +1,20 @@
+public class Printmultipal {
+
+        static void Table(int n) {
+            int i = 1;
+
+            do{
+                System.out.printf("%4d", n * i);
+                i = i + 1;
+            } while(i <= 3);
+            System.out.println("");
+        }public static void main(String[] args) {
+            int i = 1;
+            do{
+                Table(i);
+                i = i + 1;
+            } while (i <= 3
+            );
+        }
+    }
+
